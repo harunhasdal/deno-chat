a6e1bbf469c624943fb4d1a48aa8fe19aba984d4
harunhasdal <harunhasdal@gmail.com>
2021-07-06 17:21:30 +0100

    Add chat client

---
+++ chat-client/pages/index.tsx
@@ -0,0 +1,26 @@
+import { h, IS_BROWSER, useState, useEffect, useCallback  } from "../deps.ts";
+/* @jsx h */
+
+interface Message {
+  text: string;
+}
+
+export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const getMessages = useCallback(async () => {
+    const res = await fetch(`https://hhasdal-deno-chat-api.deno.dev/messages`)
+    const data = await res.json()
+    setMessages(data);
+  }, [])
+
+  useEffect(() => {
+    getMessages();
+  }, [])
+
+  return (
+    <div>
+      {JSON.stringify(messages)}
+    </div>
+  );
+}
