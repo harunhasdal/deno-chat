6f40ca56a08f5154ba4faaedda495dbfce02a57d
harunhasdal <harunhasdal@gmail.com>
2021-07-06 17:34:56 +0100

    Broadcast channel

--- chat-client/pages/index.tsx
+++ chat-client/pages/index.tsx
@@ -6,21 +6,42 @@ interface Message {
 }
 
 export default function Home() {
-  const [messages, setMessages] = useState<Message[]>([])
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [text, setText] = useState("");
 
   const getMessages = useCallback(async () => {
-    const res = await fetch(`https://hhasdal-deno-chat-api.deno.dev/messages`)
-    const data = await res.json()
+    const res = await fetch("https://hhasdal-deno-chat-api.deno.dev/messages");
+    const data = await res.json();
     setMessages(data);
-  }, [])
+  }, []);
 
   useEffect(() => {
     getMessages();
-  }, [])
+  }, []);
+
+  const onSendMessage = useCallback(async () => {
+    await fetch("https://hhasdal-deno-chat-api.deno.dev/messages", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+      }),
+    });
+    setText("");
+    getMessages();
+  }, [text]);
 
   return (
     <div>
-      {JSON.stringify(messages)}
+      <div>{JSON.stringify(messages)}</div>
+      <input
+        type="text"
+        value={text}
+        onChange={(evt) => setText(evt.target.value)}
+      />
+      <button onClick={onSendMessage}>Add</button>
     </div>
   );
 }
