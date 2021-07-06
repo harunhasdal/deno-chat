e0f1942de13af2437d3ed1d41020d87923e38af6
harunhasdal <harunhasdal@gmail.com>
2021-07-06 16:51:07 +0100

    Add Oak server

--- chat-api/server.ts
+++ chat-api/server.ts
@@ -1,6 +1,12 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World from Harun's Deno API!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+
+
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World from DENO chat API!";
 });
+
+
+addEventListener("fetch", app.fetchEventHandler());
