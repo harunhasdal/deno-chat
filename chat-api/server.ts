76ed3a1bd486fc95e0f01cb6967ca6ae2de48895
harunhasdal <harunhasdal@gmail.com>
2021-07-06 16:57:23 +0100

    Add message endpoints

--- chat-api/server.ts
+++ chat-api/server.ts
@@ -1,12 +1,24 @@
 
 
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const messages: string[] = [];
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Chat server!";
+  })
+  .get("/messages", (context) => {
+    context.response.body = messages
+  })
+  .post("/messages", async (context) => {
+    const message = await context.request.body().value
+    messages.push(message) 
+  });
 
 const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello World from DENO chat API!";
-});
-
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener("fetch", app.fetchEventHandler());
